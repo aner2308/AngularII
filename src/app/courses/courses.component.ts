95f2e0b58d6155512b87c7c0bbca788d09459eba
Anton <erikssonanton94@gmail.com>
2024-04-27 03:50:32 +0200

    Påbörjat arbete med filtreringsfunktion

--- src/app/courses/courses.component.ts
+++ src/app/courses/courses.component.ts
@@ -2,22 +2,30 @@ import { Component } from '@angular/core';
 import { Course } from '../model/course';
 import { CourseService } from '../services/course.service';
 import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-courses',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, FormsModule],
   templateUrl: './courses.component.html',
   styleUrl: './courses.component.scss'
 })
 export class CoursesComponent {
   courselist: Course[] = [];
+  filteredCourses: Course[] = [];
+  filteredValue: string = "";
 
   constructor(private courseservice: CourseService) {}
 
   ngOnInit() {
     this.courseservice.getCourses().subscribe(data => {
       this.courselist = data;
+      this.filteredCourses = data;
     })
+  }
+
+  applyFilter(): void {
+    
   }
 }
