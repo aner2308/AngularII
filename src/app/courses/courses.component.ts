534332e6d53f67cb7500a05a7ba4f08de70e2b07
Anton <erikssonanton94@gmail.com>
2024-04-27 03:22:46 +0200

    Skapat en grundstruktur utifrån Mattias Dahlgrens inspelade föreläsningar och exempel, med lite tweaks för mitt egna ändamål

---
+++ src/app/courses/courses.component.ts
@@ -0,0 +1,23 @@
+import { Component } from '@angular/core';
+import { Course } from '../model/course';
+import { CourseService } from '../services/course.service';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-courses',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './courses.component.html',
+  styleUrl: './courses.component.scss'
+})
+export class CoursesComponent {
+  courselist: Course[] = [];
+
+  constructor(private courseservice: CourseService) {}
+
+  ngOnInit() {
+    this.courseservice.getCourses().subscribe(data => {
+      this.courselist = data;
+    })
+  }
+}
